cdd81d802106b0c01d907c4d82298c4c2226eb71
Anup-UR <anup_ur7869@hotmail.com>
2022-07-20 01:01:14 +0530

    Part 3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -6,6 +6,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.lenient;
@@ -69,4 +70,14 @@ class RestaurantTest {
                 ()->restaurant.removeFromMenu("French fries"));
     }
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+    //<<<<<<<<<<<<<<<<<<<<<<<ORDER TOTAL FROM MENU FOR SELECTED ITEMS>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    @Test
+    public void order_total_from_menu_for_selected_items(){
+        int orderTotal;
+        List<Item> selectedItems=null;
+        orderTotal=restaurant.getOrderTotal(selectedItems);
+    }
+
 }
